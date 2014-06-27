589ae6eedba85c6dcd75fcd4614a39a7dab5e0bd
mikeatlas <mikeatlas@gmail.com>
2014-06-27 16:47:34 -0400

    demonstrate realtime app visually

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,8 @@
 class VisitorsController < ApplicationController
+
+	def trigger_realtime_message
+		$redis.publish 'realtime_msg', {msg: 'hello world - ' + SecureRandom.hex, recipient_user_ids: [41, 42]}.to_json
+		head :no_content
+	end
+
 end
